a437a2c80fbebdd1ac12cbafef59b393f365ff14
Tammy Nguyen <tammynguyen@berkeley.edu>
2015-10-24 12:19:24 -0400

    removed extra files

--- seed.js
+++ seed.js
@@ -1,19 +1,47 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+  Template.register.events({
+    'submit form': function(event) {
+        event.preventDefault();
+        var emailVar = event.target.registerEmail.value;
+        var passwordVar = event.target.registerPassword.value;
+        Accounts.createUser({
+            email: emailVar,
+            password: passwordVar
+        });
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.login.events({
+    'submit form': function(event){
+        event.preventDefault();
+        var emailVar = event.target.loginEmail.value;
+        var passwordVar = event.target.loginPassword.value;
+        Meteor.loginWithPassword(emailVar, passwordVar);
     }
   });
+
+  Template.dashboard.events({
+    'click .logout': function(event){
+        event.preventDefault();
+        Meteor.logout();
+    }
+  });
+
+  Template.createprofile.events({
+    'submit form':function(event){
+    var firstName = event.target.firstName.value;
+    var lastName = event.target.lastName.value;
+    var field = event.target.field.value;
+    Meteor.users.update(
+      {_id:Meteor.user()._id}, 
+      {$set: 
+        {
+          "profile.firstName":firstName, 
+          "profile.lastName":lastName,
+          "profile.field":field}
+        })
+    }
+  })
 }
 
 if (Meteor.isServer) {
@@ -21,3 +49,4 @@ if (Meteor.isServer) {
     // code to run on server at startup
   });
 }
+
